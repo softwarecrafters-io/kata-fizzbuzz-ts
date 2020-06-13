28e56e8594886d169a7bc002e04853bb898cdb7d
Miguel A Gomez <miguelghz@Mac-Pro-Software-Crafters.local>
2020-06-13 15:40:38 +0100

    Green: should get zero when recieves zero

--- src/tests/fizzbuzz.test.ts
+++ src/tests/fizzbuzz.test.ts
@@ -1,6 +1,6 @@
 import { fizzbuzz } from '../core/fizzbuzz';
 
-test('should sum two numbers', () => {
+test('should get zero when recieves zero', () => {
 	const result = fizzbuzz(0);
 	const expected = 0;
 
