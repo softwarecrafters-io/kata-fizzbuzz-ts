45fd89c447aecc89a7944b6d86743ca2b7b1858e
Miguel A Gomez <miguelghz@Mac-Pro-Software-Crafters.local>
2020-06-13 16:32:49 +0100

    Fix case

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,1 +1,1 @@
-export const fizzbuzz = (n) => 0;
+export const fizzbuzz = (n) => 1;
--- src/tests/fizzbuzz.test.ts
+++ src/tests/fizzbuzz.test.ts
@@ -1,8 +1,8 @@
 import { fizzbuzz } from '../core/fizzbuzz';
 
-test('should get zero when recieves zero', () => {
-	const result = fizzbuzz(0);
-	const expected = 0;
+test('should get one when recieves one', () => {
+	const result = fizzbuzz(1);
+	const expected = 1;
 
 	expect(result).toBe(expected);
 });
