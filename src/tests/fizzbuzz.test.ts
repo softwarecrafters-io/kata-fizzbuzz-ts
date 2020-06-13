5e78d76bad25c06bd358381ccae66f1e0cee77be
Miguel A Gomez <miguelghz@Mac-Pro-Software-Crafters.local>
2020-06-13 16:15:45 +0100

    base cases

--- src/tests/fizzbuzz.test.ts
+++ src/tests/fizzbuzz.test.ts
@@ -1,12 +1,29 @@
 import { fizzbuzz } from '../core/fizzbuzz';
 
-test('fizzbuzz should', () => {
-	const testCases = [
-		{ value: 0, expect: 0 },
-		{ value: 3, expect: 'fizz' },
-		{ value: 5, expect: 'buzz' },
-		{ value: 15, expect: 'fizzbuzz' },
-	];
+test('should get zero when recieves zero', () => {
+	const result = fizzbuzz(0);
+	const expected = 0;
 
-	testCases.forEach((testCase) => expect(fizzbuzz(testCase.value)).toBe(testCase.expect));
+	expect(result).toBe(expected);
+});
+
+test('should get fizz when recieves three', () => {
+	const result = fizzbuzz(3);
+	const expected = 'fizz';
+
+	expect(result).toBe(expected);
+});
+
+test('should get buzz when recieves five', () => {
+	const result = fizzbuzz(5);
+	const expected = 'buzz';
+
+	expect(result).toBe(expected);
+});
+
+test('should get fizzbuzz when recieves fifteen', () => {
+	const result = fizzbuzz(15);
+	const expected = 'fizzbuzz';
+
+	expect(result).toBe(expected);
 });
